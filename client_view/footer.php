979d07a546964564a0ca41277c7f4d60657e7b56
ashwani972286 <ashwani972886@gmail.com>
2021-12-19 15:16:32 +0530

    images change

--- client_view/footer.php
+++ client_view/footer.php
@@ -4,36 +4,36 @@
         <div class="container-fluid">
             <div class="footer__instagram">
 
-                <div class="footer__instagram__avatar" style="width: 250px;">
+                <!-- <div class="footer__instagram__avatar" style="width: 250px;">
 
                     <div class="footer__instagram__avatar--pic">
-                        <img src="img/footer/instagram-avatar.jpg" alt="">
+                        <img src="img/footer/vh_logo_title.png" alt="">
                     </div>
                     <div class="footer__instagram__avatar--text">
 
-                        <h5 style="font-size: 15px;">@MeeMineForever</h5>
+                        <h5 style="font-size: 15px; font-family: 'Oooh Baby', cursive;">@VisionHouse</h5>
 
                         <span>23,7k follower</span>
                     </div>
-                </div>
+                </div> -->
                 <div class="row">
                     <div class="col-lg-2 col-md-2 col-sm-4 col-6">
-                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-1.jpg"></div>
+                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-1.png"></div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-4 col-6">
-                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-2.jpg"></div>
+                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-2.png"></div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-4 col-6">
-                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-3.jpg"></div>
+                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-3.png"></div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-4 col-6">
-                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-4.jpg"></div>
+                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-4.png"></div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-4 col-6">
-                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-5.jpg"></div>
+                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-5.png"></div>
                     </div>
                     <div class="col-lg-2 col-md-2 col-sm-4 col-6">
-                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-6.jpg"></div>
+                        <div class="footer__instagram__item set-bg" data-setbg="img/footer/ip-6.png"></div>
                     </div>
                 </div>
             </div>
@@ -43,7 +43,7 @@
                 <div class="col-lg-12">
                     <div class="footer__text">
                         <div class="footer__logo">
-                            <a href="#"><img src="img/logo_vh.png" alt="Vision House"></a>
+                            <a href="?p=home"><img src="img/logo_vh.png" alt="Vision House"></a>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                             ut<br /> labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus
